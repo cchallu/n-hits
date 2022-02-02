4e929ed31e1d3ff5169b4aa0d3762a0040abb8db
cristianchallu <cristiani.challu@gmail.com>
2022-02-02 10:58:49 -0500

    evaluation

--- evaluation.py
+++ evaluation.py
@@ -19,19 +19,6 @@ def get_score_min_val(dir):
             min_mae = val_mae
     return mae_best, mse_best
 
-def get_scores_min_test(dir):
-    print(dir)
-    result = pickle.load(open(dir, 'rb'))
-    min_mae = 100
-    min_mse = 100
-    for i in range(len(result)):
-        mae = result.trials[i]['result']['test_losses']['mae']
-        mse = result.trials[i]['result']['test_losses']['mse']
-        if mae < min_mae:
-            min_mae = mae
-            min_mse = mse
-    return min_mae, min_mse
-
 def main(args):
 
     if args.horizon<0:
@@ -49,10 +36,7 @@ def main(args):
         maes = []
         mses = []
         for file_ in files:
-            if args.min == 'val':
-                mae_data, mse_data = get_score_min_val(file_)
-            if args.min == 'test':
-                mae_data, mse_data = get_scores_min_test(file_)
+            mae_data, mse_data = get_score_min_val(file_)
             maes.append(mae_data)
             mses.append(mse_data)
 
@@ -68,7 +52,6 @@ def parse_args():
     parser.add_argument('--setting', type=str, help='Multivariate or univariate', default='multivariate')
     parser.add_argument('--horizon', type=int, help='Horizon')
     parser.add_argument('--model', type=str, help='Model name')
-    parser.add_argument('--min', type=str, help='Minimum set loss', default='val')
     parser.add_argument('--experiment', type=str, help='string to identify experiment')
     return parser.parse_args()
 
