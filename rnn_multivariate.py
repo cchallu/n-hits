b72dd1ea8a1a922b3ad4509e6188d2a6c11220af
cristianchallu <cristiani.challu@gmail.com>
2022-05-15 14:56:41 -0400

    freq and smaller dasta

--- rnn_multivariate.py
+++ rnn_multivariate.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
 
     horizons = [96, 192, 336, 720]
     ILI_horizons = [24, 36, 48, 60]
-    datasets = ['Exchange'] #, 'ETTm2', 'weather', 'ili', 'ECL', 'traffic']
+    datasets = ['ETTm2', 'Exchange', 'weather', 'ili', 'ECL', 'traffic']
 
     for dataset in datasets:
         # Horizon
